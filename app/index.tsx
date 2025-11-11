65872d2cc99ea0fb094d601b1dce1888e4301322
Supun Jayawardana <supunpramudithajayawardhana@gmail.com>
2025-11-11 21:07:44 +0530

    Images in react-native

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { styles } from "../styles/auth.styles";
 
 export default function Index() {
@@ -7,13 +7,17 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.title}>Welcome</Text>
-      <TouchableOpacity onPress={ () => {alert("You touched")}}>
-        <Text>Press Me</Text>
-      </TouchableOpacity>
+      
+      <Image source={require("../assets/images/icon.png")} style={ {width: 100, height: 100 }} />
 
-      <Pressable onPress={ () => { alert('You pressed')}}>
-        <Text>Press me</Text>
-      </Pressable>
+      <Image source={{uri: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5Ym9hcmR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500"}} 
+      
+      style={{
+        width: 100,
+        height: 100,
+        resizeMode: "cover"
+      }}
+      />
 
     </View>
   );
